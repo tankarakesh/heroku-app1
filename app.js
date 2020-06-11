40016a794ffbe2c3253ed34862f33655b9b107ea
tankarakesh.38@gmail.com <Rakesh@1221>
2020-06-11 10:33:47 +0530

    Third Commit

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080, () =>
+app.use(express.static(path.join(__dirname, 'public')))
+.listen(PORT || 8080, () =>
 {
 console.log('Welcome to Heroku!');
 });
