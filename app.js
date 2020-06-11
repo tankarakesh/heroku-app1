9492beb2bdf5e98aa974ccd68656ddf909560bc4
tankarakesh.38@gmail.com <Rakesh@1221>
2020-06-11 10:42:33 +0530

    Fourth Commit

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')))
-.listen(PORT || 8080, () =>
-{
-console.log('Welcome to Heroku!');
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
 
 app.get('',(request, response) =>
