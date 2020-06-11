5562868e6257707b73a9333483adc53cd79b0156
tankarakesh.38@gmail.com <Rakesh@1221>
2020-06-11 10:59:55 +0530

    Fourth Commit

--- app.js
+++ app.js
@@ -13,8 +13,3 @@ app.get('/', function(request, response) {
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
-
-app.get('/',(request, response) =>
-{
-response.send('Welcome To Get Requests!!!');
-})
